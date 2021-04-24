b58cb5b709a29244fb830d8af05ca4510a7cbe4d
vlarysc <netosc2010@gmail.com>
2021-04-24 17:36:43 -0300

    Alunos

--- exercicio-38.js
+++ exercicio-38.js
@@ -1,7 +1,24 @@
 var turma = [
-  { Maria: [8, 5, 7, 10] },
-  { Manoel: [7, 4, 6, 8] },
-  { Renata: [3, 8, 5, 7] },
-  { Karis: [1, 9, 8, 10] },
-  { Marcelo: [5, 5, 9, 9] },
+  { nome: "Maria", nota: [8, 5, 7, 10] },
+  { nome: "Manoel", nota: [7, 4, 6, 8] },
+  { nome: "Renata", nota: [3, 8, 5, 7] },
+  { nome: "Karis", nota: [1, 9, 8, 10] },
+  { nome: "Marcelo", nota: [5, 5, 9, 9] },
 ];
+
+var texto = "";
+
+for (var i = 0; i < turma.length; i++) {
+  texto +=
+    "<p>Notas de " + turma[i].nome + ": " + turma[i].nota.join(", ") + "  </p>";
+  var nota = turma[i].nota;
+  var soma = 0;
+  for (var j = 0; j < nota.length; j++) {
+    soma += nota[j];
+  }
+  var notaMedia = soma / nota.length;
+  texto +=
+    "<p> A média anual de " + turma[i].nome + " é: " + notaMedia + " </p><hr> ";
+}
+
+document.querySelector("#output").innerHTML = texto;
