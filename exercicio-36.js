ba62ae50786624bd9528fe139c47a325e1b816f1
vlarysc <netosc2010@gmail.com>
2021-04-24 11:56:17 -0300

    ff

--- exercicio-36.js
+++ exercicio-36.js
@@ -1,19 +1,22 @@
 var nComputador = "";
 var nJogador = "";
-var minhaEscolha = "Par";
+var minhaEscolha = "";
 
 function sortear(max) {
   return Math.floor(Math.random() * max);
 }
 
 function play() {
-  if (isNaN(nJogador)) {
+  if (!nJogador) {
+    throw new Error("Precisa de um número");
+  } else if (isNaN(nJogador)) {
     throw new Error("Precisa de um número");
   } else if (nJogador === undefined) {
     throw new Error("Apenas números");
   } else {
     nJogador = document.querySelector("#Numero").value;
     nComputador = sortear(5);
+    minhaEscolha = document.querySelector('input[type="radio"]:checked').value;
     nJogador = parseInt(nJogador);
     var total = nJogador + nComputador;
     var resultado = total % 2 === 0 ? "Par" : "Ímpar";
