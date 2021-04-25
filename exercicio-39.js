f8c385a6c10156169d907ff23c70dd738a81c78e
vlarysc <netosc2010@gmail.com>
2021-04-25 09:45:02 -0300

    alunos

---
+++ exercicio-39.js
@@ -0,0 +1,62 @@
+var enderecosAlunos = [
+  {
+    nome: "Manoel",
+    id: 1,
+    endereco: new Endereco({
+      logradouro: "Rua Batista Tissot",
+      numero: 46,
+      complemento: "Casa 1",
+      cep: "95090-040",
+      cidade: "Caxias do Sul",
+      estado: "RS",
+    }),
+  },
+  {
+    nome: "Fernando",
+    id: 2,
+    endereco: new Endereco({
+      logradouro: "Rua do Conde",
+      numero: 102,
+      complemento: "Apartamento, bloco 2",
+      cep: "95090-010",
+      cidade: "Caxias do Sul",
+      estado: "SP",
+    }),
+  },
+  {
+    nome: "Renata",
+    id: 3,
+    endereco: new Endereco({
+      logradouro: "Rua 88",
+      numero: 76,
+      complemento: "Casa verde",
+      cep: "49100-000",
+      cidade: "São Cristovão",
+      estado: "SE",
+    }),
+  },
+  {
+    nome: "Debora",
+    id: 4,
+    endereco: new Endereco({
+      logradouro: "Rua Joel Geraldo Silva",
+      numero: 109,
+      complemento: "Casa 1",
+      cep: "49100-000",
+      cidade: "São Cristovão",
+      estado: "SE",
+    }),
+  },
+  {
+    nome: "Diego",
+    id: 5,
+    endereco: new Endereco({
+      logradouro: "Rua Firmino",
+      numero: 222,
+      complemento: "Pousada",
+      cep: "11963-490",
+      cidade: "Lençóis Paulista",
+      estado: "SP",
+    }),
+  },
+];
